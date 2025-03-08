36a28210c612fa7c431220bf93341b130e1f6053
Enzo Celeghini <enzoceleghini.work@gmail.com>
2025-03-08 21:57:52 +0000

    - Created a 'Portfolio' component with the total invested, a graphic image and the earnings and time filters components; - Created the 'Earnings' component  with the earnings per currency and earnings in percentage; - Created the 'TimeFilters' component for the portfolio graphic with the filters: 'Day', 'Week', 'Month', '6M (6 months)', 'YTD (year untill date)', 'Year', '5Y' (5 years) and 'Max'

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { PageHome } from './assets/components/Pages/PageHome';
+import { NavigationContainer } from "@react-navigation/native";
+
+const Navigator = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Navigator.Navigator screenOptions={{headerShown: false}}>
+        <Navigator.Screen name='Home' component={PageHome} />
+      </Navigator.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/components/pages/PageHome.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export function PageHome() {
+
+    const [totalInvested, setTotalInvested] = useState(0)
+    const [earnings, setEarnings] = useState(0)
+    const [earningsPercentage, setEarningsPercentage] = useState(0)
+
+    return(
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    )
+}
